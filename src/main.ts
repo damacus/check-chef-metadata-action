bfd584263dbba0523a59d52a0baef6428c8ead12
Dan Webb <dan.webb@damacus.io>
2022-02-17 17:08:14 +0000

    Tidy up main.ts
    Signed-off-by: Dan Webb <dan.webb@damacus.io>

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
-// import * as core from '@actions/core';
-// import * as github from '@actions/github'
-import {Message} from './messageInterface'
-import {checkMetadata} from './checkMetadata'
+import { Message } from './messageInterface'
+import { checkMetadata } from './checkMetadata'
 
 async function run(): Promise<void> {
   const checks: Message[] = []
