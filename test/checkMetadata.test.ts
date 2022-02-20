c2024bb0861d14c3e3e9fd7a9ed5e66facf1fe87
Dan Webb <dan.webb@damacus.io>
2022-02-20 14:05:41 +0000

    Add nvm configuration
    Signed-off-by: Dan Webb <dan.webb@damacus.io>

--- test/checkMetadata.test.ts
+++ test/checkMetadata.test.ts
@@ -1,8 +1,21 @@
 import {checkMetadata} from '../src/checkMetadata'
 
+const testEnvVars = {
+  INPUT_MAINTAINER: 'Sous Chefs',
+  INPUT_MAINTAINER_EMAIL: 'help@sous-chefs.org',
+  GITHUB_REPOSITORY: 'sous-chefs/java'
+}
+
 describe('Correct metadata', () => {
+  beforeEach(() => {
+    for (const key in testEnvVars)
+      process.env[key] = testEnvVars[key as keyof typeof testEnvVars]
+  })
   it('an empty validation message', async () => {
-    process.env.GITHUB_REPOSITORY = 'sous-chefs/java'
+    // beforeEach(() => {
+    //   for (const key in testEnvVars)
+    //     process.env[key] = testEnvVars[key as keyof typeof testEnvVars]
+    // })
     const message = await checkMetadata('./test/fixtures/metadata.rb')
     expect(message).toEqual({
       message: 'Metadata matches',
