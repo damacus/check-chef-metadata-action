b8a28fc34cdfa245cea6c3953e25fa7ef0e63d81
Dan Webb <dan.webb@damacus.io>
2022-02-21 16:02:18 +0000

    Add debug info warning error messages
    Signed-off-by: Dan Webb <dan.webb@damacus.io>

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+import * as core from '@actions/core'
 import {checkMetadata} from './checkMetadata'
 import {reportChecks} from './reportChecks'
 
 async function run(): Promise<void> {
   const metadataCheck = await checkMetadata()
+  core.info(`Metadata check: ${metadataCheck.message}`)
+  core.debug(`Metadata check: ${JSON.stringify(metadataCheck)}`)
+  core.error(`Metadata check: ${JSON.stringify(metadataCheck)}`)
+  core.warning(`Metadata check: ${JSON.stringify(metadataCheck)}`)
   await reportChecks(metadataCheck)
 }
 
