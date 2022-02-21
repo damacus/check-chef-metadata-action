0541a6d2135640a50c2bb50fe240858f4e65165c
Dan Webb <dan.webb@damacus.io>
2022-02-21 13:52:17 +0000

    SQUASHME

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import {checkMetadata} from './checkMetadata'
 
 async function run(): Promise<void> {
   const metadataCheck = await checkMetadata()
+  core.info('Hello from the action!')
   core.info(metadataCheck.message)
 
   if (metadataCheck.conclusion === 'failure') {
