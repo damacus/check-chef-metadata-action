9e189bf6bd310c2b890a12d9e028e64fb225b344
Dan Webb <dan.webb@damacus.io>
2022-02-22 11:16:41 +0000

    Report on the PR

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,12 @@
 import * as core from '@actions/core'
 import {checkMetadata} from './checkMetadata'
-// import {reportChecks} from './reportChecks'
-import {reportPR} from './reportPR'
+import {reportChecks} from './reportChecks'
+// import {reportPR} from './reportPR'
 
 async function run(): Promise<void> {
   try {
     const result = await checkMetadata()
-
-    await reportPR(result)
+    await reportChecks(result)
   } catch (error: unknown) {
     const err = (error as Error).message
     core.setFailed(err)
