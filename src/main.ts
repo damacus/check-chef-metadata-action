bb44cdc83fa071c40cfc55dbfcadecee2239f43e
Dan Webb <dan.webb@damacus.io>
2023-03-19 13:16:59 +0000

    Allow users to toggle reporting behaviour

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,23 @@ import {reportPR} from './reportPR'
 async function run(): Promise<void> {
   try {
     const file_path = core.getInput('file_path', {required: false})
+    const report_checks = core.getInput('report_checks', {required: false})
+    const comment_on_pr = core.getInput('comment_on_pr', {required: false})
+
     const result = await checkMetadata(file_path)
-    await reportChecks(result)
-    await reportPR(result)
+
+    if (report_checks) {
+      await reportChecks(result)
+    }
+
+    if (comment_on_pr) {
+      await reportPR(result)
+    }
+
+    // If the check failed, set the action as failed
+    if (result.conclusion === 'failure') {
+      core.setFailed('Metadata check failed')
+    }
   } catch (error: unknown) {
     const err = (error as Error).message
     core.setFailed(err)
